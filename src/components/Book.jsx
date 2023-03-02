a30de6ab897dbf96b01018fce59bd7616bc4d32b
Fithamlak <justiceofgod01@gmail.com>
2023-03-03 02:27:11 +0300

    fix linter errors

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,26 +1,16 @@
 import PropTypes from 'prop-types';
 
-const Book = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const { title, author } = props;
+const Book = ({ title, author }) => (
+  <li>
+    {title}
+    <br />
+    {author}
+    <button type="button"> Remove </button>
+  </li>
+);
 
-  return (
-    <li>
-      {title}
-      <br />
-      {author}
-      <button type="button"> Remove </button>
-    </li>
-  );
-};
-
-Book.defaultProps = {
-  title: '',
-  author: '',
-};
-
-Book.prototype = {
-  title: PropTypes.string,
-  author: PropTypes.string,
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
 };
 export default Book;
