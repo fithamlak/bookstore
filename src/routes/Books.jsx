ef6bc90a0d0fd52f3f190983153d62f67f96c049
Fithamlak <justiceofgod01@gmail.com>
2023-03-02 23:49:44 +0300

    Add Books route with sample displayied books

---
+++ src/routes/Books.jsx
@@ -0,0 +1,23 @@
+import Form from '../components/Form';
+import Book from '../components/Book';
+
+const Books = () => {
+  const books = [
+    { id: 1, title: 'The Hunger Games', author: 'Suzanne Collins' },
+    { id: 2, title: 'Dune', author: 'Frank Herbert' },
+    { id: 3, title: 'Capital in the Twenty-First Century', author: 'Suzanne Collins' },
+  ];
+  return (
+    <div>
+      <h1>List of books</h1>
+      <ul>
+        {books.map((book) => (
+          <Book key={book.id} title={book.title} author={book.author} />
+        ))}
+      </ul>
+      <Form />
+    </div>
+  );
+};
+
+export default Books;
