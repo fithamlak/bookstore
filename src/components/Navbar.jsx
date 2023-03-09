6fa24d9b87212a11f1c329beda73c4688a2b0e62
Fithamlak <justiceofgod01@gmail.com>
2023-03-09 21:01:37 +0300

    Add styling for Navbar component based on the design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,15 @@
 import { NavLink } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 import '../css/Navbar.css';
 
 const Navbar = () => (
   <header className="nav">
     <ul>
-      <li><NavLink to="/" className="link">Bookstore CMS</NavLink></li>
+      <li><NavLink to="/" className="app_logo">Bookstore CMS</NavLink></li>
       <li><NavLink to="books" className="link">Books</NavLink></li>
       <li><NavLink to="categories" className="link">Categories</NavLink></li>
     </ul>
+    <div className="avator"><FaUser /></div>
   </header>
 );
 
